67afa7249e88f7c9a9e2b184fe2140130f989bfe
geeuse <148438195+geeuse@users.noreply.github.com>
2023-11-14 11:10:09 +0100

    Add files via upload

---
+++ Notes_ReDisgn/Notes_ReDisgn/Notes_ReDisgnApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notes_ReDisgnApp.swift
+//  Notes_ReDisgn
+//
+//  Created by Giuseppe Gargiulo on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Notes_ReDisgnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
